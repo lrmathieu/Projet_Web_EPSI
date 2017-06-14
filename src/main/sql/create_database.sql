6d8bf13cc1d261b4872c951fc65be5844474b2c6
lrmathieu <lrmathieu@live.fr>
2017-06-14 14:33:02 +0200

    Réparation du fichier create_database.sql

--- src/main/sql/create_database.sql
+++ src/main/sql/create_database.sql
@@ -3,20 +3,18 @@ CREATE DATABASE mypersonnalbankweb CHARACTER SET 'utf8';
 use mypersonnalbankweb;
 
 CREATE TABLE IF NOT EXISTS Account (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(200) NOT NULL,
-  number varchar(200) NOT NULL,
-  value int(11) NOT NULL,
-  PRIMARY KEY (id)
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  number VARCHAR(50) NOT NULL,
+  value INT NOT NULL
 ) engine=innodb;
 
 CREATE TABLE IF NOT EXISTS Transaction (
-  id int(11) NOT NULL AUTO_INCREMENT,
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   transactionDate DATE NOT NULL,
-  label varchar(200) NOT NULL,
-  transactionType varchar(200) NOT NULL,
-  value int(11) NOT NULL,
-  idAccount int(11) NOT NULL,
-  PRIMARY KEY (id),
+  label VARCHAR(50) NOT NULL,
+  transactionType VARCHAR(200) NOT NULL,
+  value INT NOT NULL,
+  idAccount INT NOT NULL,
   CONSTRAINT FK_TRANS_ACC FOREIGN KEY(idAccount) REFERENCES Account(id)
-) engine=inno
+) engine=innodb;
